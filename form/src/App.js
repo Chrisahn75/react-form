46575a8640e6d9f64b356edfc963e4933a5c6c1f
Chrisahn75 <chris.ahona@gmail.com>
2022-01-31 10:51:03 +0100

    form

--- form/src/App.js
+++ form/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="d-flex flex-column align-items-center p-5">
+        <h1>Login</h1>
+        <Form className="p-5 ">
+          <Form.Group id="email">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required/>
+          </Form.Group>
+        </Form>
+      </div>
+    );
+  }
 }
 
 export default App;
