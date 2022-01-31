3f3ade04234d25a7bc2292e9aea0742b16cb9790
Chrisahn75 <chris.ahona@gmail.com>
2022-01-31 10:54:35 +0100

    regex

--- form/src/App.js
+++ form/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 class App extends React.Component {
   render() {
@@ -10,8 +11,19 @@ class App extends React.Component {
         <Form className="p-5 ">
           <Form.Group id="email">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required/>
+            <Form.Control type="email" placeholder="Enter email" pattern="^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$" required/>
           </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
         </Form>
       </div>
     );
